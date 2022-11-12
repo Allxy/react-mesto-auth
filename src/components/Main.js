174d6a14ae109cd22a1bb7f522084df13403993b
Allxy <allxy01@gmail.com>
2022-11-12 22:18:45 +0300

    feat: popup add

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,36 +4,9 @@ import Api from "../utils/Api";
 import Card from "./Card";
 
 function Main(props) {
-  const [cards, setCards] = useState([]);
   const [currentUser] = useUser();
 
-  function handleCardLike(card) {
-    const isLiked = card.likes.some((i) => i._id === currentUser._id);
 
-    Api.setLikeStatus(card._id, !isLiked)
-      .then((newCard) => {
-        const newCards = cards.map((c) => (c._id === card._id ? newCard : c));
-        setCards(newCards);
-      })
-      .catch((err) => console.error(err.message));
-  }
-
-  function handleCardDelete(card) {
-    Api.removeCard(card._id)
-      .then(() => {
-        const newCards = cards.filter((c) => c._id !== card._id);
-        setCards(newCards);
-      })
-      .catch((err) => console.error(err.message));
-  }
-
-  useEffect(() => {
-    Api.getCards()
-      .then((cards) => {
-        setCards(cards);
-      })
-      .catch((err) => console.error(err.message));
-  }, []);
 
   return (
     <main className="content">
@@ -61,13 +34,13 @@ function Main(props) {
         ></button>
       </section>
       <section aria-label="Места" className="places">
-        {cards.map((card) => (
+        {props.cards.map((card) => (
           <Card
             key={card._id}
             card={card}
             onCardClick={props.onCardClick}
-            onCardLike={handleCardLike}
-            onCardDelete={handleCardDelete}
+            onCardLike={props.onCardLike}
+            onCardDelete={props.onCardDelete}
           />
         ))}
       </section>
