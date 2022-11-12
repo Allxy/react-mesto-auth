8d88f97714570123d77e9efd7f209762b86bf66a
Allxy <allxy01@gmail.com>
2022-11-12 21:41:43 +0300

    feat: avatar and profile popup

--- src/contexts/CurrentUserContext.js
+++ src/contexts/CurrentUserContext.js
@@ -7,14 +7,18 @@ export const useUser = () => useContext(CurrentUser)
 
 const CurrentUserProvider = (props) => {
 
-  const [currentUser, setCurrentUser] = useState(null);
+  const [currentUser, setCurrentUser] = useState({
+    name: "",
+    about: "",
+    avatar: ""
+  });
 
   useEffect(() => {
     Api
       .getUser()
       .then((user) => setCurrentUser(user))
       .catch((err) => console.error(err.message));
-  });
+  },[]);
 
   return (
     <CurrentUser.Provider value={[currentUser, setCurrentUser]}>
